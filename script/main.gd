40ad561a0b1deb514503b72ac065dea3060326da
AK3 <mjlabishek123@gmail.com>
2026-04-16 12:00:24 +0545

    added a loop like mechanisham if you go to left it will take you to right and if right it will take you to left

--- script/main.gd
+++ script/main.gd
@@ -1,16 +1,59 @@
 extends Node2D
 
-@onready var player = $player
-@onready var shadow = $shadow
+@onready var player = $Player
+@onready var shadow = $Shadow
+@onready var shadow_left = $ShadowLeft
+@onready var shadow_right = $ShadowRight
 
-var positions := []
-var delay := 90
+var positions: Array[Vector2] = []
 var shadow_target := Vector2.ZERO
 
+var screen_width := 0.0
+var time_passed := 0.0
+
+
+func _ready():
+	screen_width = get_viewport_rect().size.x
+
+
 func _process(delta):
-	positions.append(player.global_position)
+	time_passed += delta
+
+	var delay := 120
+	var lerp_speed := 0.2
+
+	if time_passed < 6:
+		delay = 120
+		lerp_speed = 0.15
+	elif time_passed < 10:
+		delay = 70
+		lerp_speed = 0.25
+	else:
+		delay = 35
+		lerp_speed = 0.4
+
+
+	
+	positions.append(player.real_position)
 
 	if positions.size() > delay:
 		shadow_target = positions.pop_front()
 
-	shadow.global_position = shadow.global_position.lerp(shadow_target, 0.2)
+
+
+	shadow.global_position = shadow.global_position.lerp(shadow_target, lerp_speed)
+
+
+
+	shadow_left.global_position = shadow.global_position + Vector2(-screen_width, 0)
+	shadow_right.global_position = shadow.global_position + Vector2(screen_width, 0)
+
+
+
+	var d_main = player.global_position.distance_to(shadow.global_position)
+	var d_left = player.global_position.distance_to(shadow_left.global_position)
+	var d_right = player.global_position.distance_to(shadow_right.global_position)
+
+	shadow.visible = d_main <= d_left and d_main <= d_right
+	shadow_left.visible = d_left < d_main and d_left <= d_right
+	shadow_right.visible = d_right < d_main and d_right < d_left
--- script/player.gd
+++ script/player.gd
@@ -5,26 +5,55 @@ extends CharacterBody2D
 @export var jump_power := -400
 
 var dead := false
+var screen_width := 0.0
+
+# 🔥 REAL POSITION (never resets)
+var real_position := Vector2.ZERO
+
+
+func _ready():
+	screen_width = get_viewport_rect().size.x
+	real_position = global_position
+
 
 func _physics_process(delta):
 	if dead:
 		return
 
+	# gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+	# movement
 	var dir = Input.get_axis("left", "right")
 	velocity.x = dir * speed
 
+	# jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_power
 
 	move_and_slide()
 
+	# 🔥 update REAL position manually
+	real_position += velocity * delta
+
+	# 🔥 visual wrap only
+	wrap_visual()
+
+	# death check
 	for i in range(get_slide_collision_count()):
 		var col = get_slide_collision(i).get_collider()
-		if col and col.name.to_lower() == "shadow":
+		if col and col.name == "Shadow":
 			die()
+
+
+func wrap_visual():
+	var x = real_position.x
+
+	if x < 0:
+		global_position.x = fmod(x, screen_width) + screen_width
+	else:
+		global_position.x = fmod(x, screen_width)
 
 
 func die():
